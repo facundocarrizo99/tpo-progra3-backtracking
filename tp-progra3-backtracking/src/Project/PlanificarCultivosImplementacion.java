39bb5db991cda9a6564d573b56a70637805f4cf8
Samuel <samg.martinez2@gmail.com>
2024-11-16 14:30:04 -0300

    :construction: Empece a hacer las funciones de backtraking

--- tp-progra3-backtracking/src/Project/PlanificarCultivosImplementacion.java
+++ tp-progra3-backtracking/src/Project/PlanificarCultivosImplementacion.java
@@ -10,7 +10,23 @@ public class PlanificarCultivosImplementacion implements PlanificarCultivos {
 
     @Override
     public List<CultivoSeleccionado> obtenerPlanificacion(List<Cultivo> cultivosDisponibles, double[][] riesgos, String temporada) {
-        // TODO
+        if (riesgos == null || riesgos.length == 0) {
+            throw new IllegalArgumentException("La matriz riesgos no puede ser nula ni vacía.");
+        }
+
+        int filas = riesgos.length;
+        int columnas = riesgos[0].length;
+        int indiceCultivo = 0;
+        double mayorBeneficio = 0;
+        Cultivo[][] campo = new Cultivo[filas][columnas];
+        Cultivo[][] campoResultado = new Cultivo[filas][columnas];
+
+        obtenerPlan(cultivosDisponibles, riesgos, temporada, indiceCultivo, campo, mayorBeneficio, campoResultado);
         return null;
+    }
+
+    private double obtenerPlan(List<Cultivo> CultivosDisponibles, double[][] riesgos, String temporada, int indiceCultivo, Cultivo[][] campo, double mayorBeneficio, Cultivo[][] campoResultado){
+
+        return 0;
     }
 }
