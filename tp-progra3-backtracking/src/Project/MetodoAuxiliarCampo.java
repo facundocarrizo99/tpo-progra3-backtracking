def5ef95ede7ec6fbbbe691b7888dacc3ef69464
MariangelVillegas <mariangelarbona96@gmail.com>
2024-11-16 16:55:59 -0300

    CalcularBeneficio

--- tp-progra3-backtracking/src/Project/MetodoAuxiliarCampo.java
+++ tp-progra3-backtracking/src/Project/MetodoAuxiliarCampo.java
@@ -3,7 +3,10 @@ package Project;
 import Lib.Coordenada;
 import Lib.Cultivo;
 
-public static class  MetodoAuxiliarCampo {
+import java.util.HashSet;
+import java.util.Set;
+
+public  class  MetodoAuxiliarCampo {
 
     public static void sacarCultivo(Cultivo[][] campo, Coordenada coordenadaSuperiorIzq, Coordenada coordenadaInferiorDer){
 
@@ -22,20 +25,29 @@ public static class  MetodoAuxiliarCampo {
 
     }
 
-    public static Double calcularBeneficio(Cultivo[][] campo, Double[][] riego ){
+    public static Double calcularBeneficio(Cultivo[][] campo, Double[][] riesgo ){
         Double beneficio= 0.0;
+        Double potencialParcela = 0.0;
+        Double inversionTotal= 0.0;
+        Set<String> CultivosVisitados = new HashSet<>();
         for(int i=0;i<campo.length;i++){
             for(int j=0;j<campo.length;j++){
-
-
+                if(campo[i][j]!= null){
+                    potencialParcela = (1-riesgo[i][j]) * (campo[i][j].getPrecioDeVentaPorParcela() - campo[i][j].getCostoPorParcela());
+                    beneficio += potencialParcela;
+                    if(!CultivosVisitados.contains(campo[i][j].getNombre())){
+                        CultivosVisitados.add(campo[i][j].getNombre());
+                        inversionTotal+= campo[i][j].getInversionRequerida();
+                    }
+                }
             }
-
         }
+        beneficio = beneficio -inversionTotal;
+
+
         return beneficio;
 
     }
-
-
 
 }
 
